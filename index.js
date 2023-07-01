ec76758caace7749a5ada23d1ab517282cf8f7ff
Oyinkansola Busayomi <125446283+Bousandie@users.noreply.github.com>
2023-07-01 14:03:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let totalHomeScore = 0
+let totalGuestScore = 0
+let home = document.getElementById("totalHScore");
+let guest = document.getElementById("totalGScore");
+home.textContent = totalHomeScore;
+guest.textContent = totalGuestScore;
+
+function homePointOne() {
+    totalHomeScore += 1
+    home.innerText = totalHomeScore
+}
+function homePointTwo() {
+    totalHomeScore += 2
+    home.innerText = totalHomeScore;
+}
+function homePointThree() {
+    totalHomeScore += 3
+    home.innerText = totalHomeScore
+    
+} 
+function guestPointOne() {
+    totalGuestScore += 1
+    guest.innerText = totalGuestScore
+}
+function guestPointTwo() {
+    totalGuestScore += 2
+    guest.innerText = totalGuestScore
+}
+function guestPointThree() {
+    totalGuestScore += 3
+    guest.innerText = totalGuestScore
+}
